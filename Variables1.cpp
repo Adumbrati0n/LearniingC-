051a052cfbcb720fe92fd737e062a86e391442e2
Adumbration <58345633+Adumbrati0n@users.noreply.github.com>
2021-04-26 11:04:56 +1000

    Create Variables1.cpp

---
+++ Variables1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  int x;
+  float y;
+
+  x = 50;
+  y = 2.5;
+  y = (y + 231)/6;
+  std::cout << x << "\n";
+  std::cout << y << "\n";
+
+  std::cout << (x + y) * 32 << "\n";
+
+  float dsjafl = x - y + 11.1;
+  std::cout << dsjafl << "\n";
+  int Rem = x + y;
+  std::cout << Rem << "\n";
+  return 0;
+}
