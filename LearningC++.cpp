b59dcdf7a4a37ce03f812d7c4c8daddbd1b63f77
Adumbration <58345633+Adumbrati0n@users.noreply.github.com>
2021-04-26 10:53:46 +1000

    Create LearningC++.cpp

---
+++ LearningC++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Code here
+
+
+  cout << "Learning C++ >:D" << " According to all known laws of aviation, there is no way a bee should be able to fly. It's wings are too small to get its fat little body off the ground. The bee of course flies anyway. " << "Lmao. "; 
+  cout << "\n";
+  cout << "\n";
+  cout << "Price: " << (5 * 25000) - 54 << " " << "USD";
+  cout << "\nValue: $" << 5 * 200;
+
+  return 0;
+  // End Code here
+}
